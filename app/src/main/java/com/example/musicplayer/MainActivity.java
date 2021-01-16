b87098e6306b984c9775643a3c5ce719756711c6
PaceyJA98 <72321951+PaceyJA98@users.noreply.github.com>
2021-01-16 14:15:06 -0600

    2nd Commit - Added mediaplayer

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -2,13 +2,57 @@ package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button play,pause,stop;
+    MediaPlayer mediaPlayer;
+    int pauseCurrentPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        play = (Button) findViewById(R.id.btnplay);
+        pause = (Button) findViewById(R.id.btnpause);
+        stop = (Button) findViewById(R.id.btnstop);
+
+        play.setOnClickListener(this);
+        pause.setOnClickListener(this);
+        stop.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btnplay:
+                if(mediaPlayer==null){
+                mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.crookedstill);
+                mediaPlayer.start();
+                }
+                else if(!mediaPlayer.isPlaying()){
+                    mediaPlayer.seekTo(pauseCurrentPosition);
+                    mediaPlayer.start();
+                }
+                break;
+
+            case R.id.btnpause:
+                if(mediaPlayer!=null){
+                    mediaPlayer.pause();
+                    pauseCurrentPosition=mediaPlayer.getCurrentPosition();
+                }
+                break;
+
+            case R.id.btnstop:
+                if(mediaPlayer!=null) {
+                    mediaPlayer.stop();
+                    mediaPlayer = null;
+                }
+                break;
+        }
     }
 }
